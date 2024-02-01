7b8009004e956ac9692dd149dda12131c84324ea
hoheesu <hoheesu@naver.com>
2024-02-01 20:50:15 +0900

    Feat: 회원가입 페이지 기본 구성중

--- static/signup.js
+++ static/signup.js
@@ -1,1 +1,16 @@
-window.document.getElementById("");
+const duplicationBtn = document.getElementById("duplication");
+const newUserId = document.getElementById("user_id");
+
+const idDuplicationCheck = () => {
+  fetch("/url 입력", {
+    method: "POST",
+    // headers: {},
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+};
+
+duplicationBtn.addEventListener("click", () => {
+  // idDuplicationCheck()
+});
